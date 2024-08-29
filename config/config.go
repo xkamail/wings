708cdd0ba8a1db4014898b67a3e6b380b9d94ea6
Daniel Barton <danielb@purpleflaghosting.com>
2024-08-29 09:48:57 +0800

    chore: reorder config loading to load config, *then* fill in missing defaults (#185)

--- config/config.go
+++ config/config.go
@@ -351,12 +351,6 @@ type Configuration struct {
 // This function does not modify the currently stored global configuration.
 func NewAtPath(path string) (*Configuration, error) {
 	var c Configuration
-	// Configures the default values for many of the configuration options present
-	// in the structs. Values set in the configuration file take priority over the
-	// default values.
-	if err := defaults.Set(&c); err != nil {
-		return nil, err
-	}
 	// Track the location where we created this configuration.
 	c.path = path
 	return &c, nil
@@ -560,6 +554,13 @@ func FromFile(path string) error {
 	}
 
 	if err := yaml.Unmarshal(b, c); err != nil {
+		return err
+	}
+
+	// Configures the default values for many of the configuration options present
+	// in the structs. Values set in the configuration file will not be overridden by the
+	// default values.
+	if err := defaults.Set(c); err != nil {
 		return err
 	}
 
